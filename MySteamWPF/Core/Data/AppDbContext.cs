c215757c5eb5f390899d6d87b86a5d9fbe9d8ee2
Necrohrich <elshanisayev378@gmail.com>
2025-08-11 18:58:33 +0400

    DbContext added

---
+++ MySteamWPF/Core/Data/AppDbContext.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using MySteamWPF.Core.Models;
+
+namespace MySteamWPF.Core.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Game> Games { get; set; } = null!;
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<Comment> Comments { get; set; } = null!;
+    public DbSet<GameTag> GameTags { get; set; } = null!;
+    public DbSet<UserGame> UserGames { get; set; } = null!;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Composite key for many-to-many tables
+        modelBuilder.Entity<GameTag>().HasKey(gt => new { gt.GameId, gt.Tag });
+        modelBuilder.Entity<UserGame>().HasKey(ug => new { ug.UserId, ug.GameId });
+
+        // Relationships
+        modelBuilder.Entity<GameTag>()
+            .HasOne(gt => gt.Game)
+            .WithMany(g => g.Tags)
+            .HasForeignKey(gt => gt.GameId);
+
+        modelBuilder.Entity<UserGame>()
+            .HasOne(ug => ug.User)
+            .WithMany(u => u.UserGames)
+            .HasForeignKey(ug => ug.UserId);
+
+        modelBuilder.Entity<UserGame>()
+            .HasOne(ug => ug.Game)
+            .WithMany()
+            .HasForeignKey(ug => ug.GameId);
+    }
+}
